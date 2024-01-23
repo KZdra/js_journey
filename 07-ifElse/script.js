7373702fb8543480656fcdc5728a61adb3853378
IndraHardika <motorantapani@gmail.com>
2024-01-23 21:30:39 +0700

    IF ELSEEE #11

---
+++ 07-ifElse/script.js
@@ -0,0 +1,23 @@
+// IF ELse 
+
+// var angka = 3
+
+// if (angka == 3) {
+//  console.log('angka benar') 
+// } else {
+//     console.log('angka salah')
+// }
+// -----------------------
+
+// var angka = 12
+
+// if (angka == 10) {
+//     console.log('angka yang dimasukan adalah 10')
+//     if (angka < 12) {
+//         console.log('angka lebih kecil dari 12')
+//     }
+// } else {
+//     console.log('angka gak tepat')
+// }
+//----------------------- if else tapi if dijero if 
+
